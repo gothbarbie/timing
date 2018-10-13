0bcd1165a051ea40e677c3a6001e9b02271027ea
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-13 16:11:48 +0200

    chore: add icon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faArrowCircleLeft,
   faArrowCircleRight,
+  faEllipsisH,
 } from '@fortawesome/free-solid-svg-icons'
 
 import Root from './Root'
@@ -11,7 +12,7 @@ import App from './App/App'
 
 import store from './store'
 
-library.add(faArrowCircleLeft, faArrowCircleRight)
+library.add(faArrowCircleLeft, faArrowCircleRight, faEllipsisH)
 
 ReactDOM.render(
   <Root store={store}>
