3b06d79379b91133dfcd02036755de9c9b6dd530
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-13 12:19:25 +0200

    refactor: Root-component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
 
+import Root from './Root'
 import App from './App/App'
 
 import store from './store'
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Root store={store}>
     <App />
-  </Provider>,
+  </Root>,
   document.getElementById('root')
 )
