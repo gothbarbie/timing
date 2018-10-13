20e9d95a04f4f569764511fe934f7cd13b27577e
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-13 15:25:05 +0200

    chore: create Icon

---
+++ src/generic/Icon/Icon.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// NOTE! All icons used must be manually added at src/index.js
+
+const Icon = ({ icon }) => <FontAwesomeIcon icon={icon} />
+
+export default Icon
