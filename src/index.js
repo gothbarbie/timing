e20fb770344657883d9e88fc13a3a7f2cda63e8f
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-13 15:24:01 +0200

    chore: add used icons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faArrowCircleLeft,
+  faArrowCircleRight,
+} from '@fortawesome/free-solid-svg-icons'
 
 import Root from './Root'
 import App from './App/App'
 
 import store from './store'
+
+library.add(faArrowCircleLeft, faArrowCircleRight)
 
 ReactDOM.render(
   <Root store={store}>
