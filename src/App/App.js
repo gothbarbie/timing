ace99534b971a3c61d96caeeea99c941af4cd911
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-13 15:24:21 +0200

    chore: add Home view

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,16 @@ import React from 'react'
 import Theme from '../generic/Theme/Theme'
 import Normalize from '../generic/Normalize/Normalize'
 import MainHeader from '../MainHeader'
+import Home from '../views/Home'
 
 const App = () => (
   <React.Fragment>
     <Normalize />
     <Theme>
-      <MainHeader />
+      <React.Fragment>
+        <MainHeader />
+        <Home />
+      </React.Fragment>
     </Theme>
   </React.Fragment>
 )
