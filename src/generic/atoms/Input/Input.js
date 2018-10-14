99b2692c94a5c9e5123a6a7f2e1368bb1d72d8de
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-14 12:59:44 +0200

    style: size, padding and color

--- src/generic/atoms/Input/Input.js
+++ src/generic/atoms/Input/Input.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 const Input = styled.input`
   border-radius: 4px;
   border: 1px solid ${({ theme }) => theme.colors.gray};
+  padding: 1rem 1.5rem;
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.colors.dark};
 `
 
 export default Input
