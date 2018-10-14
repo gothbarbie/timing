0116c80e0584162585471b081693634d6f75e9b7
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-14 13:00:29 +0200

    chore: fix broken refs

--- src/generic/molecules/LabeledInput/LabeledInput.js
+++ src/generic/molecules/LabeledInput/LabeledInput.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import Label from 'generic/Label/Label'
-import Input from 'generic/Input/Input'
+import Label from 'generic/atoms/Label/Label'
+import Input from 'generic/atoms/Input/Input'
 
 const LabeledInputStyle = styled.div`
   display: flex;
   flex-direction: column;
 `
 
+const LabelStyled = styled(Label)`
+  margin-bottom: 1rem;
+`
+
 const LabeledInput = ({ name, label, onChange }) => (
   <LabeledInputStyle>
-    <Label htmlFor={name}>{label}</Label>
+    <LabelStyled htmlFor={name}>{label}</LabelStyled>
     <Input name={name} onChange={onChange} />
   </LabeledInputStyle>
 )
