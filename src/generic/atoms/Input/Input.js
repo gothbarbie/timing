78bb4be0cb7ba78bbd2ed3e9870d51dab25b3e67
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-14 13:54:30 +0200

    style: fix focus and add props

--- src/generic/atoms/Input/Input.js
+++ src/generic/atoms/Input/Input.js
@@ -1,11 +1,21 @@
+import React from 'react'
 import styled from 'styled-components'
 
-const Input = styled.input`
+const InputStyle = styled.input`
   border-radius: 4px;
   border: 1px solid ${({ theme }) => theme.colors.gray};
   padding: 1rem 1.5rem;
   font-size: 1.6rem;
   color: ${({ theme }) => theme.colors.dark};
+  outline: none;
+
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+  }
 `
+
+const Input = ({ name, type = 'text' }) => {
+  return <InputStyle name={name} type={type} />
+}
 
 export default Input
