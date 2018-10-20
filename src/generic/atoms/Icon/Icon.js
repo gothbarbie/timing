015c54e53161d03522a6b8ab55d30ab941e685ab
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-20 09:48:49 +0200

    style: add gray as color option

--- src/generic/atoms/Icon/Icon.js
+++ src/generic/atoms/Icon/Icon.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const IconStyle = styled(FontAwesomeIcon)`
   color: ${({ color, theme }) => color === 'white' && theme.colors.white};
+  color: ${({ color, theme }) => color === 'gray' && theme.colors.gray};
 `
 
 const Icon = ({ color, icon, size }) => (
