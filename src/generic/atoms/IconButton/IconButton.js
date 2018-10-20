946a6e8f76232c8699cc6f42d3b8bb88b40daf82
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-20 11:01:33 +0200

    feat: new IconButton component

---
+++ src/generic/atoms/IconButton/IconButton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const ButtonStyle = styled.button`
+  padding: 0;
+  margin: 0;
+  outline: none;
+  background-color: transparent;
+  border: none;
+  cursor: ${({ onClick }) => (onClick ? 'pointer' : 'none')};
+  color: ${({ color, theme }) => color === 'white' && theme.colors.white};
+  color: ${({ color, theme }) => color === 'gray' && theme.colors.gray};
+
+  &:active,
+  &:hover,
+  &:focus {
+    color: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme, hoverColor }) =>
+      hoverColor === 'gray' && theme.colors.gray};
+  }
+`
+
+// NOTE! All icons used must be manually added at src/index.js
+const IconButton = ({ color, hoverColor, icon, size = 'lg', onClick }) => (
+  <ButtonStyle onClick={onClick} color={color} hoverColor={hoverColor}>
+    <FontAwesomeIcon icon={icon} size={size} />
+  </ButtonStyle>
+)
+
+export default IconButton
