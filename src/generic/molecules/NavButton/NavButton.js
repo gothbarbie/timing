60d6a4afb8e2eadc014c7cb7c19f3718bd338482
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-20 09:02:30 +0200

    style: add pointer and alignment

--- src/generic/molecules/NavButton/NavButton.js
+++ src/generic/molecules/NavButton/NavButton.js
@@ -7,12 +7,22 @@ const NavButtonStyle = styled.button`
   border: none;
   background-color: transparent;
   color: ${({ theme }) => theme.colors.primary};
+  cursor: pointer;
   font-size: 3rem;
   padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover,
+  &:active {
+    color: ${({ theme }) => theme.colors.gray};
+  }
 `
 
-const NavButton = ({ direction = 'left' }) => (
-  <NavButtonStyle>
+const NavButton = ({ direction = 'left', onClick, title }) => (
+  <NavButtonStyle onClick={onClick} title={title}>
     {direction === 'left' && <Icon icon={['fas', 'arrow-circle-left']} />}
     {direction === 'right' && <Icon icon={['fas', 'arrow-circle-right']} />}
   </NavButtonStyle>
