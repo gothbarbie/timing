5eed5b765ca54cd4de9884e46e625d7d1e01b22a
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-20 15:55:24 +0200

    feat: allow to open modal with specific content

--- src/pages/Home/components/Modals/AddEvent/AddEvent.js
+++ src/pages/Home/components/Modals/AddEvent/AddEvent.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
+
+import { openModal } from '../../../../../generic/molecules/Modal/modalActions'
 
 import Button from '../../../../../generic/atoms/Button'
 
 import LabeledInput from '../../../../../generic/molecules/LabeledInput'
 import LabeledSelect from '../../../../../generic/molecules/LabeledSelect'
 import LabeledTags from '../../../../../generic/molecules/LabeledTags'
-
-const Wrapper = styled.section``
 
 const TwoColumns = styled.div`
   display: grid;
@@ -31,38 +32,51 @@ const AlignRight = styled.div`
   width: 100%;
 `
 
-const AddEvent = () => (
-  <Wrapper>
-    <LabeledInput name="title" label="Title" />
+const AddEvent = ({ openModal }) => (
+  <section>
+    <LabeledInput label="Title" name="title" />
     <TwoColumns>
-      <LabeledInput name="start" label="Start" type="date" />
-      <LabeledInput name="end" label="End" type="date" />
+      <LabeledInput label="Start" name="start" type="date" />
+      <LabeledInput label="End" name="end" type="date" />
     </TwoColumns>
     <CategorySection>
       <LabeledSelect
-        name="category"
         label="Category"
+        name="category"
         options={[{ key: 'Test', value: 'TestValue' }]}
       />
-      <Button primary>Edit</Button>
+      <Button
+        fullWidth
+        onClick={() => openModal({ content: 'editCategories' })}
+        primary
+      >
+        Edit
+      </Button>
     </CategorySection>
     <TwoColumns>
-      <LabeledInput name="add-tags" label="Add tags" />
+      <LabeledInput label="Add tags" name="add-tags" />
       <LabeledTags
-        tags={[{ blue: true, name: 'unit test', id: 1 }]}
         label="Used recently"
         onClick={id => console.log('added', id)}
+        tags={[{ blue: true, name: 'unit test', id: 1 }]}
       />
     </TwoColumns>
     <LabeledTags
-      tags={[{ blue: false, name: 'story-125', id: 3 }]}
       label="Selected tags"
       onClick={id => console.log('deleted', id)}
+      tags={[{ blue: false, name: 'story-125', id: 3 }]}
     />
     <AlignRight>
       <Button primary>Create</Button>
     </AlignRight>
-  </Wrapper>
+  </section>
 )
 
-export default AddEvent
+const mapDispatchToProps = {
+  openModal,
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(AddEvent)
