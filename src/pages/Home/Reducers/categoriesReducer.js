f9be5ee78808fe832d31cecd603da4b2e31e7b65
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-20 09:03:03 +0200

    feat: new categoriesReducer

---
+++ src/pages/Home/Reducers/categoriesReducer.js
@@ -0,0 +1,10 @@
+const initialState = ['Category Name']
+
+const categoriesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default categoriesReducer
