d5a6a889ad7f27f4bece541fdf6626cbf41137a4
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-20 09:01:19 +0200

    style: fix height issue

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Theme from 'generic/Theme/Theme'
 import Normalize from 'generic/Normalize/Normalize'
 import MainHeader from 'layout/MainHeader'
 import Home from 'pages/Home'
 
+const AppWrap = styled.div`
+  height: 100vh;
+`
+
 const App = () => (
   <React.Fragment>
     <Normalize />
     <Theme>
-      <React.Fragment>
+      <AppWrap>
         <MainHeader />
         <Home />
-      </React.Fragment>
+      </AppWrap>
     </Theme>
   </React.Fragment>
 )
